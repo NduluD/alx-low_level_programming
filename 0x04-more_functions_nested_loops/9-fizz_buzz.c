d875dee6fe7dd5eb8488aa39f6e66b891c3e0208
NduluD <nduluwambua24@gmail.com>
2023-09-14 16:20:02 -0700

    Fizz-Buzz tes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if ((i % 5 == 0) && (i % 3 != 0))
 		{
 			printf("Buzz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
@@ -28,5 +28,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
